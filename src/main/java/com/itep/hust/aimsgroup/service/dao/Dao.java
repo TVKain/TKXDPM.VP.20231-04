920cdc92d6f8389bd9306c67ebd5bd33495eada6
tvkain <tvkain.it@gmail.com>
2023-12-04 16:47:26 +0700

    feature: redesign dao class

--- src/main/java/com/itep/hust/aimsgroup/service/dao/Dao.java
+++ src/main/java/com/itep/hust/aimsgroup/service/dao/Dao.java
@@ -6,9 +6,9 @@ import java.util.List;
  * Dao pattern to access object in the database
  * @param <T>
  */
-public interface Dao<T> {
+public interface Dao<T,U> {
     public List<T> getAll();
-    public T get(Integer id);
+    public T get(U id);
     public T insert(T t);
     public T update(T t);
     public T delete(T t);
