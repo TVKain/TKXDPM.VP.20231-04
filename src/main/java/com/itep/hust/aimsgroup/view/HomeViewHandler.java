1e834f91c8a975de86d4c1bda15714e8f2ec5632
Remilia Scarlet <lethanhbinhkts@gmail.com>
2023-12-05 00:41:31 +0700

    Feature: Implement searching on homepage

--- src/main/java/com/itep/hust/aimsgroup/view/HomeViewHandler.java
+++ src/main/java/com/itep/hust/aimsgroup/view/HomeViewHandler.java
@@ -3,10 +3,15 @@ package com.itep.hust.aimsgroup.view;
 import com.itep.hust.aimsgroup.controller.HomeController;
 import com.itep.hust.aimsgroup.model.media.Media;
 import com.itep.hust.aimsgroup.service.dao.SqliteMediaDao;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -46,24 +51,70 @@ public class HomeViewHandler {
     private VBox vboxMedia3;
 
     @FXML
+    private TextField searchBox;
+
+    @FXML
     private VBox vboxMedia4;
 
     private HomeController homeController = new HomeController();
 
+    public HomeViewHandler() {
+    }
+
     @FXML
     public void initialize() {
-        List<Media> lstMedia = new SqliteMediaDao().getAll();
+        // Sửa List thành ObservableList để lắng nghe thay đổi
+        ObservableList<Media> lstMedia = FXCollections.observableArrayList();
+        lstMedia.addAll(new SqliteMediaDao().getAll());
+
         // Tạo danh sách VBox để lưu trữ các cột
         List<VBox> vboxColumns = new ArrayList<>();
         vboxColumns.add(vboxMedia1);
         vboxColumns.add(vboxMedia2);
         vboxColumns.add(vboxMedia3);
         vboxColumns.add(vboxMedia4); // Thêm vào danh sách
-
         int i = 0;
         while (i < lstMedia.size()) {
             VBox vboxColumn = vboxColumns.get(i%4);
             try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/home/media_home.fxml"));
+                MediaHomeViewHandler mediaHomeViewHandler = new MediaHomeViewHandler(lstMedia.get(i));
+                loader.setController(mediaHomeViewHandler);
+                loader.load();
+                vboxColumn.getChildren().add(loader.getRoot());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
+        splitMenuBtnSearch.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                if (searchBox.getText() != null){
+                    int i = 0;
+                    while (i< lstMedia.size()){
+                        if ( !lstMedia.get(i).getTitle().contains(searchBox.getCharacters())){
+                            System.out.println(lstMedia.get(i).getTitle());
+                            lstMedia.remove(lstMedia.get(i));
+                        }
+                        i++;
+                    }
+                }
+            }
+        });
+    }
+
+    @FXML
+    public void handleSearch(){
+
+    }
+
+    @FXML
+    public void refresh(ObservableList<Media> lstMedia, List<VBox> vboxColumns  ){
+        int i = 0;
+        while (i < lstMedia.size()) {
+            try {
+                VBox vboxColumn = vboxColumns.get(i%4);
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/home/media_home.fxml"));
                 MediaHomeViewHandler mediaHomeViewHandler = new MediaHomeViewHandler(lstMedia.get(i));
                 loader.setController(mediaHomeViewHandler);
