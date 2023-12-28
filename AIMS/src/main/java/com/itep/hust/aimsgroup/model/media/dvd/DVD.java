cebc89a09ae440ff87eb1ca7ffc21e4936629b3a
Khanh <khanhngocdam>
2023-12-28 07:19:38 +0700

    Update: Model to show media in manager screen

---
+++ AIMS/src/main/java/com/itep/hust/aimsgroup/model/media/dvd/DVD.java
@@ -0,0 +1,26 @@
+package com.itep.hust.aimsgroup.model.media.dvd;
+
+import com.itep.hust.aimsgroup.model.media.Media;
+
+import java.util.Date;
+
+public class DVD extends Media {
+    String discType;
+    String director;
+    int runtime;
+    String studio;
+    String subtitles;
+    String language;
+    Date releasedDate; //Null
+    String DVDCategory; // NULL
+
+    public DVD(int id, String title, String category, int price, int value, int quantity, double weight, String imageURL, String discType, String director, int runtime, String studio, String subtitles) {
+        super(id, title, category, price, value, quantity, weight, imageURL);
+        this.discType = discType;
+        this.category = category;
+        this.director = director;
+        this.runtime = runtime;
+        this.studio = studio;
+        this.subtitles = subtitles;
+    }
+}
