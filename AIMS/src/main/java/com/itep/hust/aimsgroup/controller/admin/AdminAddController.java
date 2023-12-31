8f7ce10194be26b7b1f9b4ca870895ebb3b55bea
tvkain <tvkain.it@gmail.com>
2023-12-31 16:30:03 +0700

    feature: added admin create mail

--- AIMS/src/main/java/com/itep/hust/aimsgroup/controller/admin/AdminAddController.java
+++ AIMS/src/main/java/com/itep/hust/aimsgroup/controller/admin/AdminAddController.java
@@ -1,9 +1,11 @@
 package com.itep.hust.aimsgroup.controller.admin;
 
+import com.itep.hust.aimsgroup.controller.admin.mail.AdminCreateMail;
 import com.itep.hust.aimsgroup.controller.admin.validator.AccountAddValidator;
 import com.itep.hust.aimsgroup.controller.admin.validator.AccountValidator;
 import com.itep.hust.aimsgroup.model.account.Account;
 import com.itep.hust.aimsgroup.service.dao.AccountDao;
+import com.itep.hust.aimsgroup.service.email.JavaMailUtil;
 import com.itep.hust.aimsgroup.util.Popup;
 
 public class AdminAddController {
@@ -21,6 +23,16 @@ public class AdminAddController {
 
         accountDao.insert(account);
         Popup.showSuccess("Account created successfully");
+
+        String content = AdminCreateMail.getContent(account);
+
+        try {
+            JavaMailUtil.sendMail(account.getEmail(), content);
+        } catch (Exception e) {
+            System.out.println("Send mail fail");
+            System.out.println(e.getMessage());
+        }
+
 
         return true;
     }
